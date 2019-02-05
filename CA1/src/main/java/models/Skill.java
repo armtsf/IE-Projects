3dc9acd9ccbdecf8eb8492a6c6600e3794608e76
Armita Safa <armt.safa@gmail.com>
2019-02-05 18:04:00 +0330

    added Skill class

---
+++ CA1/src/main/java/models/Skill.java
@@ -0,0 +1,27 @@
+package models;
+
+public class Skill {
+    private String name;
+    private int points;
+
+    public Skill(String name, int points) {
+        this.name = name;
+        this.points = points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
