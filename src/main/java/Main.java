d8f5855801eafa79a4452d70e727910fbb797410
Shayan Hosseini <shayan.hosseiny@gmail.com>
2019-02-21 19:05:42 +0330

    WIP: change Main class for CA2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,71 +1,18 @@
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpServer;
 import javafx.util.Pair;
 import models.*;
 
 import java.io.IOException;
 import java.io.InvalidObjectException;
+import java.net.InetSocketAddress;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
-    private static boolean isFinished = false;
-    private static ObjectMapper mapper = new ObjectMapper();
-
     public static void main(String[] args) throws IOException {
-        while (!isFinished) {
-            Pair<String, String> commandParts = getCommandParts();
-            String commandName = commandParts.getKey();
-            String commandData = commandParts.getValue();
-
-            switch (commandName) {
-                case "register":
-                    User user = mapper.readValue(commandData, User.class);
-                    try {
-                        UserList.add(user);
-                    } catch (InvalidObjectException e) {
-                        System.err.println(e.getMessage());
-                    }
-                    break;
-                case "addProject":
-                    Project project = mapper.readValue(commandData, Project.class);
-                    try {
-                        ProjectList.add(project);
-                    } catch (InvalidObjectException e) {
-                        System.err.println(e.getMessage());
-                    }
-                    break;
-                case "bid":
-                    Bid bid = mapper.readValue(commandData, Bid.class);
-                    try {
-                        BidList.add(bid);
-                    } catch (InvalidObjectException e) {
-                        System.err.println(e.getMessage());
-                    }
-                    break;
-                case "auction":
-                    isFinished = true;
-                    JsonNode jsonNode = mapper.readTree(commandData);
-                    String projectTitle = jsonNode.get("projectTitle").asText();
-                    try {
-                        User winner = Auction.finish(projectTitle);
-                        if (winner != null) {
-                            System.out.println("winner: " + winner.getUsername());
-                        } else {
-                            System.out.println("no winner!");
-                        }
-                    } catch (NoSuchElementException e) {
-                        System.err.println("invalid projectTitle");
-                    }
-                    break;
-            }
-        }
-    }
-
-    private static Pair<String, String> getCommandParts() {
-        String command = scanner.nextLine();
-        int spaceIndex = command.indexOf(" ");
-        return new Pair<>(command.substring(0, spaceIndex), command.substring(spaceIndex));
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/project");
     }
 }
