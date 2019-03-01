d4dff4e81011ee6d26741b6fa021140ece5fd52a
Armita Safa <armt.safa@gmail.com>
2019-03-01 20:35:24 +0330

    implemented user list page

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -3,11 +3,18 @@ package service;
 import models.User;
 import models.UserList;
 
+import java.util.ArrayList;
+
 public class UserService {
 
     public static User getUser(String userId) {
         User requestedUser = UserList.get(userId);
         return requestedUser;
+    }
+
+    public static ArrayList<User> getUsersList(User user) {
+        ArrayList<User> userList = UserList.getAllExcept(user);
+        return userList;
     }
 
 }
