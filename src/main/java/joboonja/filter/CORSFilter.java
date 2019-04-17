ed588e567cebfe5b5cd3fa1d6ebff6900f41dead
Shayan Hosseini <shayan.hosseiny@gmail.com>
2019-04-17 21:39:36 +0430

    allow all headers in CORS

--- src/main/java/joboonja/filter/CORSFilter.java
+++ src/main/java/joboonja/filter/CORSFilter.java
@@ -17,6 +17,7 @@ public class CORSFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
 
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "*");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
 
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
